0631a3169f5383658f40b6ed00fd0db8e537edcb
MIchael McKee <mike@dorkydata.com>
2023-07-02 09:30:58 -0400

    Collected Tags Data

--- get_tags_subscribers.py
+++ get_tags_subscribers.py
@@ -11,11 +11,21 @@ with open('SUBSCRIBER_DATA/list_of_tags.json', 'r') as file:
     tag_info = []
 
     for i in tags:
-        response = requests.get(f'https://api.convertkit.com/v3/tags/{i["id"]}/subscriptions?api_secret=<{api_secret}>')
+        response = requests.get(f'https://api.convertkit.com/v3/tags/{i["id"]}/subscriptions?api_secret={api_secret}')
         r = response.json()
-        temp = {i['name']:r}
-        tag_info.append(temp)
+        if r['total_pages'] > 1:
+            ran = int(r['total_pages'])
+            for j in range (1, ran + 1):
+                new_response = requests.get(f'https://api.convertkit.com/v3/tags/{i["id"]}/subscriptions?api_secret={api_secret}&page={j}')
+                new_r = new_response.json()
+                temp = {i['name']:new_r}
+                tag_info.append(temp)
+        else:
+            temp = {i['name']:r}
+            tag_info.append(temp)
 
-print(tag_info)
+with open('SUBSCRIBER_DATA/tags_data.json', 'w') as new_file:
+    json.dump(tag_info, new_file, indent=4)
+
 
 print('-----------------SUCCESS-----------------')
